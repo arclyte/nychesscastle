32712fa4fa1a074495978af89870c3a2d293010d
arclyte <arclyte@gmail.com>
2014-02-16 00:04:44 -0500

    Remove _site folder, should be ignored

--- _site/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	nav_id = nav_id || "";
-
-	if (nav_id !== "nav-home" && nav_id !== "") {
-		$('nav li.active').removeClass('active');
-		$('#' + nav_id).addClass('active');
-	}
-});
